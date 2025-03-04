5a96c5cfd56d510ee6d6e7b1e8d727fbd7d5b1fe
ayaanqui <ayaanqui.com@gmail.com>
2025-03-03 23:28:33 -0600

    test: update product test

--- tests/product_test.go
+++ tests/product_test.go
@@ -26,13 +26,22 @@ func TestProduct(t *testing.T) {
 	})
 
 	t.Run("find product", func(t *testing.T) {
-		found_product, err := service.FindProductWithCode(ctx, input.Code)
-		if err != nil {
-			t.Fatal(err)
-		}
+		t.Run("find product with correct barcode", func(t *testing.T) {
+			found_product, err := service.FindProductWithCode(ctx, input.Code)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		if !reflect.DeepEqual(found_product, product) {
-			t.Fatal("products not equal", found_product, product)
-		}
+			if !reflect.DeepEqual(found_product, product) {
+				t.Fatal("products not equal", found_product, product)
+			}
+		})
+
+		t.Run("find product with incorrect barcode", func(t *testing.T) {
+			_, err := service.FindProductWithCode(ctx, "01234567810")
+			if err == nil {
+				t.Fatal("product should throw an error")
+			}
+		})
 	})
 }
