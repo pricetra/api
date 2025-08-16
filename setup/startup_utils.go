9968878e18c60de9cc7731d510e0acd6e1d8bd30
ayaanqui <ayaanqui.com@gmail.com>
2025-08-16 16:56:35 -0500

    feat: migrate script generation

--- setup/startup_utils.go
+++ setup/startup_utils.go
@@ -3,11 +3,14 @@ package setup
 import (
 	"context"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"github.com/go-jet/jet/v2/postgres"
 	"github.com/pricetra/api/database/jet/postgres/public/table"
 	"github.com/pricetra/api/services"
+	"github.com/pricetra/api/utils"
 )
 
 func MigrateAllRemoteProductImagesToCDN(service services.Service) {
@@ -37,5 +40,69 @@ func MigrateAllRemoteProductImagesToCDN(service services.Service) {
 	}
 }
 
+func MigrateFillWeightTypeColumn(service services.Service) {
+	ctx := context.Background()
+	qb := table.Product.
+		SELECT(
+			table.Product.ID.AS("id"),
+			table.Product.Weight.AS("weight"),
+		).
+		WHERE(
+			postgres.AND(
+				table.Product.Weight.IS_NOT_NULL(),
+				table.Product.Weight.NOT_EQ(postgres.String("")),
+			),
+		).
+		ORDER_BY(table.Product.ID)
+	var weights []struct {
+		ID     int64 `sql:"primary_key"`
+		Weight string
+	}
+	if err := qb.QueryContext(ctx, service.DB, &weights); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	log.Printf("Found %d products with weight to migrate", len(weights))
+
+	var stmts []string
+	for _, w := range weights {
+		weight_comps, err := utils.ParseWeightIntoStruct(w.Weight)
+		if err != nil {
+			log.Printf("Failed to parse weight for product ID %d: %v", w.ID, err)
+			continue
+		}
+		stmts = append(
+			stmts, 
+			strings.ReplaceAll(strings.ReplaceAll(
+				table.Product.
+					UPDATE(
+						table.Product.WeightType,
+						table.Product.WeightValue,
+					).
+					SET(
+						table.Product.WeightType.SET(postgres.String(weight_comps.WeightType)),
+						table.Product.WeightValue.SET(postgres.Float(weight_comps.Weight)),
+					).
+					WHERE(table.Product.ID.EQ(postgres.Int64(w.ID))).
+					DebugSql(),
+				"    ",
+				" ",
+			), "\n", " "),
+		)
+	}
+	file, err := os.OpenFile("database/migrations/1755381239971867_migrate_product_weights_and_units.sql", os.O_APPEND | os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	
+	if _, err := file.WriteString(strings.Join(stmts, "\n")); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
 func StartupUtils(service services.Service) {
+	MigrateFillWeightTypeColumn(service)
 }
