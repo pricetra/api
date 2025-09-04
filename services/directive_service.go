85a2dfca8c27d1bff5248a6856a955460025f159
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2025-09-04 17:00:14 -0500

    Bugfix/Authorization context
    Move authorization check in the middleware instead of the directive step. This allows us to check if a user is authenticated even when the `@isAuthenticated` directive is not used. This is because we verify/store the user object at the /graphql middleware level.

--- services/directive_service.go
+++ services/directive_service.go
@@ -6,22 +6,16 @@ import (
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/pricetra/api/graph/gmodel"
-	"github.com/pricetra/api/types"
 )
 
 func (s Service) IsAuthenticatedDirective(ctx context.Context, obj any, next graphql.Resolver, role *gmodel.UserRole) (res any, err error) {
-	authorization, ok := ctx.Value(types.AuthorizationKey).(types.AuthorizationKeyType)
-	if !ok {
-		return nil, fmt.Errorf("authorization header type error")
-	}
-
-	user, err := s.VerifyJwt(ctx, authorization)
-	if err != nil {
+	user := s.GetAuthUserFromContext(ctx)
+	if user == (gmodel.User{}) {
 		return nil, fmt.Errorf("unauthorized")
 	}
+
 	if role != nil && !s.IsRoleAuthorized(*role, user.Role) {
 		return nil, fmt.Errorf("insufficient permissions")
 	}
-	new_ctx := context.WithValue(ctx, types.AuthUserKey, user)
-	return next(new_ctx)
+	return next(ctx)
 }
--- services/middleware_service.go
+++ services/middleware_service.go
@@ -4,19 +4,30 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/pricetra/api/graph/gmodel"
 	"github.com/pricetra/api/types"
 )
 
 // Extracts the value from "Authorization" header and stores
 // it within the request context, with key `types.AuthorizationKey`
-func (Service) AuthorizationMiddleware(next http.Handler) http.Handler {
+func (s Service) AuthorizationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorization := r.Header.Get("Authorization")
+		bearer_token := types.AuthorizationKeyType(authorization)
 		r = r.WithContext(context.WithValue(
 			r.Context(), 
 			types.AuthorizationKey, 
-			types.AuthorizationKeyType(authorization),
+			bearer_token,
 		))
+
+		// If valid JWT token, store the user info in context with key `types.AuthUserKey`
+		if user, err := s.VerifyJwt(r.Context(), bearer_token); err == nil && user != (gmodel.User{}) {
+			r = r.WithContext(context.WithValue(
+				r.Context(), 
+				types.AuthUserKey, 
+				user,
+			))
+		}
 		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
