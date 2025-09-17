7c58ec549f0544aae766d95c53bbbcfb3ca62e31
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2025-09-16 19:15:36 -0500

    bugfix/Issues with inaccurate price creation date (#58)

--- services/price_service.go
+++ services/price_service.go
@@ -95,6 +95,8 @@ func (s Service) CreatePrice(ctx context.Context, user gmodel.User, input gmodel
 		table.Price.ExpiresAt,
 		table.Price.CreatedByID,
 		table.Price.UpdatedByID,
+		table.Price.CreatedAt,
+		table.Price.UpdatedAt,
 	).MODEL(model.Price{
 		Amount: input.Amount,
 		CurrencyCode: currency_code,
@@ -110,6 +112,8 @@ func (s Service) CreatePrice(ctx context.Context, user gmodel.User, input gmodel
 		ExpiresAt: input.ExpiresAt,
 		CreatedByID: &user.ID,
 		UpdatedByID: &user.ID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
 	}).RETURNING(table.Price.AllColumns)
 	if err = qb.QueryContext(ctx, s.TX, &price); err != nil {
 		return gmodel.Price{}, err
@@ -277,11 +281,11 @@ func (s Service) PaginatedPrices(
 		if filters.OrderBy != nil {
 			switch *filters.OrderBy {
 				case gmodel.OrderByTypeAsc:
-					order_by = table.Price.CreatedAt.ASC()
+					order_by = table.Price.ID.ASC()
 				case gmodel.OrderByTypeDesc:
-					order_by = table.Price.CreatedAt.DESC()
+					order_by = table.Price.ID.DESC()
 				default:
-					order_by = table.Price.CreatedAt.DESC()
+					order_by = table.Price.ID.DESC()
 			}
 		}
 	}
